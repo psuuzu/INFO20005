1244e00fa9fff5aef44b4ee795f6ffbde729116e
psuuzu <sup1@student.unimelb.edu.au>
2024-05-29 20:51:21 +1000

    description of imagecycle

--- javascript/Imagecycle.js
+++ javascript/Imagecycle.js
@@ -1,19 +1,19 @@
 let images = ["files/promo/promo1.jpg", "files/promo/promo2.jpg", "files/promo/promo3.jpg", "files/promo/promo4.jpg", "files/promo/promo5.jpg",]
-
+//first image starts with first source of the array, second starts with the second...
 let firstimage=0
 let secondimage=1
 let thirdimage=2
 
 function imagecycle(){
-    firstimage = (firstimage+1)%images.length
+    firstimage = (firstimage+1)%images.length //increase array index
     secondimage = (secondimage+1)%images.length
     thirdimage = (thirdimage+1)%images.length
 
-    let imageElement1 = document.getElementById("promo1")
+    let imageElement1 = document.getElementById("promo1") //get elements
     let imageElement2 = document.getElementById("promo2")
     let imageElement3 = document.getElementById("promo3")
 
-    imageElement1.src = images[firstimage]
+    imageElement1.src = images[firstimage]   //change image source
     imageElement2.src = images[secondimage]
     imageElement3.src = images[thirdimage]
 }
